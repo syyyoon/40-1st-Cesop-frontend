f0778db98324b1eb68c9cbd239b9826dbf7f58f1
ppnj0213 <ppnj0213@gmail.com>
2022-12-06 11:03:13 +0900

    [40기 박나정] - Add : Banner layout

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,10 +4,10 @@ import './Banner.scss';
 const Banner = () => {
     return (
         <div className="Banner">
-            <div className="AlertBanner">
-                <ul className="AlertList">
-                    <li className="AlertBody">
-                        <p className="AlertLink">
+            <div className="alertBanner">
+                <ul className="alertList">
+                    <li className="alertBody">
+                        <p className="alertLink">
                             <a>
                                 포스트-푸 드롭스와 오일 버너 블렌드 제품 자진
                                 회수 안내
@@ -16,9 +16,9 @@ const Banner = () => {
                     </li>
                 </ul>
             </div>
-            <div className="ShippingBanner">
-                <div className="ShippingContent">
-                    <button className="ShippingButton">
+            <div className="shippingBanner">
+                <div className="shippingContent">
+                    <button className="shippingButton">
                         <p>전 구매 무료 배송과 선물포장 혜택을 즐겨보세요</p>
                     </button>
                 </div>
