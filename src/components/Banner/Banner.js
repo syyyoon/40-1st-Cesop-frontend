028d7a047fe653ecbf7fdb415b02c6bafe672682
ppnj0213 <ppnj0213@gmail.com>
2022-12-09 01:38:51 +0900

    [40기 박나정] - MODIFY : Banner 레이아웃 scss 전체 수정 및 mixin.scss 수정

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './Banner.scss';
+import '../../styles/mixin.scss';
 
 const Banner = () => {
     return (
-        <div className="Banner">
+        <div className="banner">
             <div className="alertBanner">
                 <ul className="alertList">
                     <li className="alertBody">
-                        <p className="alertLink">
-                            <a>
-                                포스트-푸 드롭스와 오일 버너 블렌드 제품 자진
-                                회수 안내
-                            </a>
+                        <p>
+                            포스트-푸 드롭스와 오일 버너 블렌드 제품 자진 회수
+                            안내
                         </p>
                     </li>
                 </ul>
@@ -19,7 +18,9 @@ const Banner = () => {
             <div className="shippingBanner">
                 <div className="shippingContent">
                     <button className="shippingButton">
-                        <p>전 구매 무료 배송과 선물포장 혜택을 즐겨보세요</p>
+                        <p className="shippingMesssage">
+                            전 구매 무료 배송과 선물포장 혜택을 즐겨보세요.
+                        </p>
                     </button>
                 </div>
             </div>
--- src/components/Nav/Nav.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Nav.scss';
-
-const Nav = () => {
-    return <div className="Nav">Nav</div>;
-};
-
-export default Nav;
