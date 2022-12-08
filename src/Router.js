1402f263afcde0a80ef6e0ba38d7bd9add1c27ce
YOON <yoon@YOONui-MacBookPro.local>
2022-12-08 14:28:02 +0900

    [40기 윤선영] MODIFY : 회원가입 레이아웃 수정사항 반영

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Cart from './pages/Cart/Cart';
-// import DetailList from './pages/DetailList/DetailList';
-// import DetailProducts from './pages/DetailProducts/DetailProducts';
-// import Login from './pages/Login/Login';
-// import Main from './pages/Main/Main';
-// import Payment from './pages/Payment/Payment';
+import Cart from './pages/Cart/Cart';
+import DetailList from './pages/DetailList/DetailList';
+import DetailProducts from './pages/DetailProducts/DetailProducts';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import Payment from './pages/Payment/Payment';
 import SignUp from './pages/SignUp/SignUp';
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* <Route path="/" element={<Main />} />
-                <Route path="/login" element={<Login />} /> */}
+                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
-                {/* <Route path="/detail-list" element={<DetailList />} />
+                <Route path="/detail-list" element={<DetailList />} />
                 <Route path="/detail-products" element={<DetailProducts />} />
                 <Route path="/cart" element={<Cart />} />
-                <Route path="/payment" element={<Payment />} /> */}
+                <Route path="/payment" element={<Payment />} />
             </Routes>
         </BrowserRouter>
     );
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './SignUp.scss';
+import { SIGNUP_TWO_INPUT_LIST, SIGNUP_ONE_INPUT_LIST } from './SignupData';
 
 const SignUp = () => {
     return (
@@ -18,23 +19,39 @@ const SignUp = () => {
                     </span>
                 </div>
 
-                <div className="wrappingInputName">
-                    <div className="lastName">
-                        <input type="text" placeholder="성" />
-                    </div>
-                    <div className="firstName">
-                        <input type="text" placeholder="이름" />
-                    </div>
-                </div>
-                <div className="wrappingInput">
-                    <input type="text" placeholder="이메일 주소" />
-                </div>
-                <div className="wrappingInput">
-                    <input type="text" placeholder="비밀번호" />
-                    <button>보기</button>
-                </div>
+                {SIGNUP_TWO_INPUT_LIST.map(list => {
+                    return (
+                        <div className={list.wrapper}>
+                            <input
+                                className={list.userInfo.input1_className}
+                                type={list.userInfo.type1}
+                                placeholder={list.userInfo.value1}
+                            />
+                            <input
+                                className={list.userInfo.input2_className}
+                                type={list.userInfo.type2}
+                                placeholder={list.userInfo.value2}
+                            />
+                        </div>
+                    );
+                })}
 
-                <div className="checkBox">
+                {SIGNUP_ONE_INPUT_LIST.map(list => {
+                    return (
+                        <div className={list.wrapper}>
+                            <input
+                                className={list.className}
+                                type={list.type}
+                                placeholder={list.value}
+                            />
+                            <button className="splitPasswordHide">
+                                {list.showButton}
+                            </button>
+                        </div>
+                    );
+                })}
+
+                <div className="userCheckBox">
                     <div className="checkList">
                         <div>
                             <input
@@ -43,6 +60,7 @@ const SignUp = () => {
                                 name=""
                                 value=""
                             />
+                            <img src="" alt="" />
                             &nbsp;본인은 14세 이상입니다 (필수)
                         </div>
                         <div>
