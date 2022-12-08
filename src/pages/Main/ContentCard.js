f770ee90919b0869eef95585cf853063c33fe9c8
ppnj0213 <ppnj0213@gmail.com>
2022-12-08 15:06:30 +0900

    [40기 박나정] - MODIFY : scss nesting | Main page Card 속성 수정

--- src/pages/Main/ContentCard.js
+++ src/pages/Main/ContentCard.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './ContentCard.scss';
 
-const ContentCard = ({ title, info, more }) => {
+const ContentCard = props => {
+    const { img, title, contents } = props;
     return (
-        <section className="contentCard">
-            <h1>{title}</h1>
-            <div className="content">
-                <div className="contentTop">
-                    <p>{info}</p>
-                </div>
-                <div className="contentBottom">
-                    <a>{more}</a>
+        <div className="wrapper">
+            <div className="banner">
+                <img className="leftImage" src={img} alt="cardImg" />
+            </div>
+            <div className="leftContent">
+                <div className="contentTitle">
+                    <section className="contentCard">
+                        <h1 className="title">{title}</h1>
+                        <div className="content">
+                            <div className="contentTop">
+                                <p>{contents.text}</p>
+                            </div>
+                            <div className="contentBottom">
+                                <a className="contentMore">{contents.more}</a>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
-        </section>
+        </div>
     );
 };
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContentCard from './ContentCard';
 import './Main.scss';
+import { CARD_DATA_BOTTOM, CARD_DATA_TOP } from './CardData';
 
 const Main = () => {
     return (
@@ -14,12 +15,23 @@ const Main = () => {
                             alt="logo"
                         />
                         <div className="contentTitle">
-                            <ContentCard
-                                title="선물을 받는 모든 분들을 위해"
-                                info="엄선된 다양한 기프트 중, 받으시는 분의 유형과 성향에
-                                어울리는 선물을 찾아보세요."
-                                more="기프트 가이드 보기"
-                            />
+                            <section className="contentCard">
+                                <h1 className="title">
+                                    선물을 받는 모든 분들을 위해
+                                </h1>
+                                <div className="content">
+                                    <div className="contentTop">
+                                        <p>
+                                            엄선된 다양한 기프트 중, 받으시는
+                                            분의 유형과 성향에 어울리는 선물을
+                                            찾아보세요.
+                                        </p>
+                                    </div>
+                                    <div className="contentBottom">
+                                        <a>기프트 가이드 보기</a>
+                                    </div>
+                                </div>
+                            </section>
                         </div>
                     </div>
                     <div className="mainBanner">
@@ -30,64 +42,18 @@ const Main = () => {
                         />
                     </div>
                 </div>
-
-                <div className="wrapper">
-                    <div className="banner">
-                        <img
-                            className="leftImage"
-                            src="../../images/main2.png"
-                            alt="main2"
+                {CARD_DATA_TOP.map(cards => {
+                    console.log(cards);
+                    return (
+                        <ContentCard
+                            key={cards.id}
+                            img={cards.img}
+                            title={cards.title}
+                            contents={cards.contents}
                         />
-                    </div>
-                    <div className="leftContent">
-                        <div className="contentTitle">
-                            <ContentCard
-                                title="장소의 시학"
-                                info="전 세계에 있는 주요 이솝 스토어 네 곳의 건축과 주변의 분위기를 담은 이솝의 디자인 유산에서 영감을 받아 탄생한 새로운 기프트 키트 컬렉션."
-                                more="시즈널 기프트 키트 보기"
-                            />
-                        </div>
-                    </div>
-                </div>
+                    );
+                })}
 
-                <div className="wrapper">
-                    <div className="incenseContent">
-                        <div className="contentTitle">
-                            <ContentCard
-                                title="홈 케어를 위한 기프트—인센스"
-                                info="섬세하고 은은하게 퍼지는 향기가 집이나 편안하게 머무는 장소에 평온한 안식을 선사합니다."
-                                more="인센스 보기"
-                            />
-                        </div>
-                    </div>
-                    <div className="mainBanner">
-                        <img
-                            className="bannerImage"
-                            src="../../images/main3.png"
-                            alt="main3"
-                        />
-                    </div>
-                </div>
-
-                <div className="wrapper">
-                    <div className="banner">
-                        <img
-                            className="leftImage"
-                            src="../../images/main4.png"
-                            alt="main2"
-                        />
-                    </div>
-                    <div className="leftContent">
-                        <div className="contentTitle">
-                            <ContentCard
-                                title="향수"
-                                info="휠, 테싯, 마라케시 인텐스, 로즈—우디에서 플로럴에 이르기까지 밤과 낮에 모두 어울리는 현대적인 작품들"
-                                more="향수 보기"
-                            />
-                        </div>
-                    </div>
-                </div>
-                {/*임시 컴포넌트*/}
                 <h1
                     style={{
                         textAlign: 'center',
@@ -97,26 +63,17 @@ const Main = () => {
                 >
                     캐러셀 컴포넌트 추가 예정
                 </h1>
-
-                <div className="wrapper">
-                    <div className="incenseContent">
-                        <div className="contentTitle">
-                            <ContentCard
-                                title="다양한 지원 모드"
-                                info="타인을 위한 선물이든 자신에게 건네는 선물이든 이솝에서는 1:1 채팅 상담, 이메일, 전화 상담 등 다양한 채널을 통해 컨설팅과 지원을 받을 수 있습니다."
-                                more="이솝 서비스 살펴보기"
-                            />
-                        </div>
-                    </div>
-                    <div className="mainBanner">
-                        <img
-                            className="bannerImage"
-                            src="../../images/main5.jpg"
-                            alt="main5"
+                {CARD_DATA_BOTTOM.map(cards => {
+                    return (
+                        <ContentCard
+                            key={cards.id}
+                            img={cards.img}
+                            title={cards.title}
+                            contents={cards.contents}
                         />
-                    </div>
-                </div>
-                {/*임시 컴포넌트*/}
+                    );
+                })}
+
                 <h1
                     style={{
                         textAlign: 'center',
@@ -126,7 +83,6 @@ const Main = () => {
                 >
                     캐러셀 컴포넌트 추가 예정
                 </h1>
-                {/*임시 컴포넌트*/}
                 <h1
                     style={{
                         textAlign: 'center',
