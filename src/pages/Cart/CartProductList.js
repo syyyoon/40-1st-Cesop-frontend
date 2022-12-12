c1964dfff079e9c6fff7629fcec8629425937518
hurjwon <jwh2700@gmail.com>
2022-12-12 11:30:19 +0900

    [40기 허재원] Add: cart 제품 목록 상수데이터, 삭제버튼 기능구현

--- src/pages/Cart/CartProductList.js
+++ src/pages/Cart/CartProductList.js
@@ -1,28 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
 import './CartProductList.scss';
+import PRODUCT from './PRODUCT';
 
 const CartProductList = () => {
-    return (
-        <li className="productItem">
-            <div className="productName">레저렉션 아로마틱 핸드 워시</div>
-            <div className="productSize">
-                <span>500mL (펌프 포함)</span>
-            </div>
-            <div className="QuantityButtonWrapper">
-                <select name="productQuantity" id="selectQuantity">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-            </div>
-            <button className="deleteQuantity">삭제</button>
-            <div className="totalAmount">
-                <span>₩141,000</span>
-            </div>
-        </li>
-    );
+    const [remove, setRemove] = useState(PRODUCT);
+
+    const onRemove = id => {
+        setRemove(remove.filter(list => list.id !== id));
+    };
+    console.log(remove);
+    return remove.map(list => {
+        return (
+            <li className="productItem" key={list.id}>
+                <div className="productName">{list.productName}</div>
+                <div className="productSize">{list.productSize}</div>
+                <div className="QuantityButtonWrapper">
+                    <select name="productQuantity" id="selectQuantity">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+                <button
+                    className="deleteQuantity"
+                    onClick={() => onRemove(list.id)}
+                >
+                    삭제
+                </button>
+                <div className="totalAmount">{list.productAmount}</div>
+            </li>
+        );
+    });
 };
 
 export default CartProductList;
