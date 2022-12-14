7ccde0decb345fc227d1eabf449e66b203bcf0d7
hurjwon <jwh2700@gmail.com>
2022-12-14 15:18:43 +0900

    [40기 허재원] 리뷰 반영 수정

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Cart.scss';
 import '../../styles/mixin.scss';
 import PRODUCT from './PRODUCT';
 import CartProductList from './CartProductList';
 
 const Cart = () => {
-    const [carts, setCarts] = useState(PRODUCT);
+    const [carts, setCarts] = useState([]);
+
+    useEffect(() => {
+        fetch('/data/product.json', {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then(data => setCarts(data));
+    }, []);
 
     const total = carts
         .reduce((a, b) => a + b.productPrice * b.amount, 0)
+        // 숫자 3자리 단위 마다 ,를 찍는 정규표현식 코드입니다.
         .toString()
         .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 
