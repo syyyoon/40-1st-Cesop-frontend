1a40533b848389a2677cc2c367bc10d95dc2fec5
YOON <yoon@YOONui-MacBookPro.local>
2022-12-14 23:36:29 +0900

    [40기 윤선영] MODIFY : config.js 파일 수정

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://10.58.52.204:8000';
-const API = {
+export const BASE_URL = 'http://10.58.52.204:8000';
+export const API = {
     signin: `${BASE_URL}/users/signin`,
     signup: `${BASE_URL}/users/signup`,
     list: `${BASE_URL}/products/all`,
